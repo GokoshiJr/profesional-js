a45d2d2de986cf8036fbfe2efc7d3930ca61271e
GokoshiJr <julioagonzalez18@gmail.com>
2020-10-30 15:31:03 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Platzi - Curso Profesional de JS
+
+## Install
+
+```git
+$ git clone https://github.com/GokoshiJr/profesional-js.git
+```
+
+## Contributing
+
+If someone wants to add or improve something, I invite you to collaborate directly in this repository: [profesional-js](https://github.com/GokoshiJr/profesional-js).
+
+## License
+
+profesional-js is released under the [MIT License](https://opensource.org/licenses/MIT).
+
+## Resources
+
+## Author
+
+**[GokoshiJr](https://github.com/GokoshiJr)** (Julio Gonz&aacute;lez) Ing Computaci&oacute;n UJAP 2020-2CR.
---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+// para reproducir el video, boton play/pause
+
+function MediaPlayer(config) {
+  this.media = config.el;
+}
+
+MediaPlayer.prototype.play = function () {
+  this.media.play();
+};
+
+MediaPlayer.prototype.pause = function () {
+  this.media.pause();
+};
+
+MediaPlayer.prototype.togglePlay = function () {
+  if (this.media.paused) {
+    this.play();
+  } else {
+    this.pause();
+  }
+};
+
+const video = document.querySelector('video');
+const player = new MediaPlayer({el: video});
+
+const button = document.querySelector('button');
+button.onclick = () => player.togglePlay();
